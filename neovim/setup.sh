b541abe326688fe6341ab36fbe36cec9167d35a8
Carl <carlwhittick@gmail.com>
2022-12-20 22:50:15 +0000

    Tidy dirs and ignore config

--- neovim/setup.sh
+++ neovim/setup.sh
@@ -2,22 +2,28 @@
 
 BASE_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)
 
+get_latest_github_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
 #################################
 # Install SouceCodePro NerdFont #
 #################################
-mkdir -p ~/.local/share/fonts
+mkdir -p $HOME/.local/share/fonts
 
 LATEST_RELEASE_VERSION=$(get_latest_github_release "ryanoasis/nerd-fonts")
 
 # Only download a new version if it doesn't match the old one
-if test -f ~/.local/share/fonts/SourceCodePro/version && [ "$(cat ~/.local/share/fonts/SourceCodePro/version)" == "$LATEST_RELEASE_VERSION" ]; then
-  echo "Already up to date"
+if test -f $HOME/.local/share/fonts/SourceCodePro/version && [ "$(cat $HOME/.local/share/fonts/SourceCodePro/version)" == "$LATEST_RELEASE_VERSION" ]; then
+  echo "Already up to date."
 else
-  mkdir -p ~/.local/share/fonts/SourceCodePro
+  mkdir -p $HOME/.local/share/fonts/SourceCodePro
   curl -o /tmp/SourceCodePro.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/$LATEST_RELEASE_VERSION/SourceCodePro.zip
   # Use the freshen option to only replace new files on extract
-  unzip -f /tmp/SourceCodePro.zip -d ~/.local/share/fonts/SourceCodePro
-  echo $LATEST_RELEASE_VERSION > ~/.local/share/fonts/SourceCodePro/version
+  unzip -f /tmp/SourceCodePro.zip -d $HOME/.local/share/fonts/SourceCodePro
+  echo $LATEST_RELEASE_VERSION > $HOME/.local/share/fonts/SourceCodePro/version
 fi
 
 # Refresh the font cache
@@ -28,21 +34,20 @@ fc-cache
 ###################
 
 # Install Astronvim base
-if [ -d "$BASE_DIR/neovim/nvim" ]; then
-  git -C "$BASE_DIR/neovim/nvim" pull
+if [ -d "$BASE_DIR/nvim" ]; then
+  git -C "$BASE_DIR/nvim" pull
 else
-  git clone https://github.com/AstroNvim/AstroNvim $BASE_DIR/neovim/nvim
+  git clone https://github.com/AstroNvim/AstroNvim $BASE_DIR/nvim
 fi
 
 # Install custom Astronvim config
-git clone https://github.com/carlwhittick/astronvim_config $BASE_DIR/neovim/nvim/lua/user
-if [ -d "$BASE_DIR/neovim/nvim/lua/user" ]; then
-  git -C "$BASE_DIR/neovim/nvim/lua/user" pull
+if [ -d "$BASE_DIR/nvim/lua/user" ]; then
+  git -C "$BASE_DIR/nvim/lua/user" pull
 else
-  git clone https://github.com/carlwhittick/astronvim_config $BASE_DIR/neovim/nvim/lua/user
+  git clone https://github.com/carlwhittick/astronvim_config $BASE_DIR/nvim/lua/user
 fi
 
 ############
 # Symlinks #
 ############
-sudo ln -sf $BASE_DIR/neovim/nvim $HOME/.config/nvim
+sudo ln -sf $BASE_DIR/nvim $HOME/.config/nvim
