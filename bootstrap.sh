0575c17228e4917e38405263c9d440bd5bc0804f
Carl <carlwhittick@gmail.com>
2022-12-20 23:32:30 +0000

    Fix package install error when there were no items to install

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@
 
 BASE_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)
 
+
 ./backup.sh
 
+./packages/setup.sh
 ./tmux/setup.sh
 ./neovim/setup.sh
 ./sxhkd/setup.sh
--- packages/setup.sh
+++ packages/setup.sh
@@ -2,12 +2,15 @@
 
 BASE_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)
 
+pacman -Qq > /tmp/installed-pacman-packages
+yay -Qq > /tmp/installed-yay-packages
+
 ###########################
 # Install Pacman packages #
 ###########################
-cat $BASE_DIR/pacman.list | grep -vxFf <(pacman -Qq) | xargs sudo pacman -S --noconfirm
+cat $BASE_DIR/pacman.list | grep -vxFf /tmp/installed-pacman-packages | xargs -r sudo pacman -S --noconfirm
 
 ########################
 # Install Yay packages #
 ########################
-cat $BASE_DIR/yay.list | grep -vxFf <(pacman -Qq) | xargs yay -S --noconfirm
+cat $BASE_DIR/yay.list | grep -vxFf /tmp/installed-yay-packages | xargs -r yay -S --noconfirm
